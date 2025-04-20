6db0dc93c06896b4916a304af21f8e821c6a6d3f
Anya Lin <hukk1996@gmail.com>
2025-04-20 15:57:30 +0800

    add redstone_torch,repeater,comparator,observer,piston,sticky_piston,daylight_detector

--- config.py
+++ config.py
@@ -57,11 +57,18 @@ LANG = {
     "${activation_duration}block.minecraft.light_weighted_pressure_plate": "{lang.value} {glyph.clock}0.5s",
     "${activation_duration}block.minecraft.heavy_weighted_pressure_plate": "{lang.value} {glyph.clock}0.5s",
     # Other Redstone Components (signal duration)
+    "${activation_duration}block.minecraft.redstone_torch": "{lang.value} {glyph.clock}0.1s",
+    "${activation_duration}block.minecraft.repeater": "{lang.value} {glyph.clock}0.1s",
+    "${activation_duration}block.minecraft.comparator": "{lang.value} {glyph.clock}0.1s",
+    "${activation_duration}block.minecraft.observer": "{lang.value} {glyph.clock}0.1s",
     "${activation_duration}block.minecraft.lectern": "{lang.value} {glyph.clock}0.1s",
+    "${activation_duration}block.minecraft.piston": "{lang.value} {glyph.clock}0.1s,0.15s",
+    "${activation_duration}block.minecraft.sticky_piston": "{lang.value} {glyph.clock}0.1s,0.15s",
     "${activation_duration}block.minecraft.lightning_rod": "{lang.value} {glyph.clock}0.4s",
     "${activation_duration}block.minecraft.target": "{lang.value} {glyph.clock}0.4s,1s",
     "${activation_duration}block.minecraft.tripwire_hook": "{lang.value} {glyph.clock}0.5s",
     "${activation_duration}block.minecraft.detector_rail": "{lang.value} {glyph.clock}1s",
+    "${activation_duration}block.minecraft.daylight_detector": "{lang.value} {glyph.clock}1s",
     # Other Redstone Components (signal duration & comparator chart)
     "${sculk_both}block.minecraft.sculk_sensor": "{lang.value} {glyph.clock}1.5s{align.left}{format.white}{glyph.sculk_sensor_guide}",
     "+${sculk_chart}block.minecraft.sculk_sensor": "{lang.value}{align.left}{format.white}{glyph.sculk_sensor_guide}",
